dd6be908a44a3a6b6e165c39c5f29d9dee6b8864
Luca A. <92426198+lucale1114@users.noreply.github.com>
2025-01-09 12:15:38 +0100

    stuff

--- Assets/Scripts/Player/BirdMovement.cs
+++ Assets/Scripts/Player/BirdMovement.cs
@@ -1,13 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
 public class BirdMovement : MonoBehaviour
 {
+    readonly float TERMINAL_VELOCITY = -6;
     private GameObject birdBase;
     private Rigidbody2D rb2d;
 
-    public float floatPower;
+    public float FloatPower;
+    public float BirdSpeed;
+
+    private bool isFalling;
 
     private void Awake()
     {
@@ -21,16 +26,33 @@ public class BirdMovement : MonoBehaviour
 
     private void DoAFlap()
     {
+        birdBase.transform.DOKill();
+        birdBase.transform.DORotate(new Vector3(0, 0, 20), 0.1f);
         rb2d.velocity = Vector3.zero;
-        rb2d.AddForce(new Vector3(0, floatPower, 0));
+        rb2d.AddForce(new Vector3(0, FloatPower, 0));
     }
 
     // Update is called once per frame
     void Update()
     {
+        Camera.main.transform.position = new Vector3(birdBase.transform.position.x + 1.2f, 0, -10);
+        if (rb2d.velocity.y <= TERMINAL_VELOCITY)
+        {
+            if (!isFalling)
+            {
+                isFalling = true;
+                birdBase.transform.DORotate(new Vector3(5, 0, -90), 0.5f)   ;
+            }
+            rb2d.velocity = new Vector3(0, TERMINAL_VELOCITY, 0);
+        } 
+        var vel = rb2d.velocity;
+        vel.x = BirdSpeed;
+        rb2d.velocity = vel;
+
         if (Input.touchCount > 0) {
             if (Input.GetTouch(0).phase == TouchPhase.Began)
             {
+                isFalling = false;
                 DoAFlap();
             }
         }
