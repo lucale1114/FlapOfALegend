394172989d3a37edcfecf1f7afe45e99224b2b69
Luca A. <92426198+lucale1114@users.noreply.github.com>
2025-01-16 07:09:18 +0100

    Brick

--- Assets/Scripts/Stage/PipeExit.cs
+++ Assets/Scripts/Stage/PipeExit.cs
@@ -9,6 +9,33 @@ public class PipeExit : MonoBehaviour
 {
     public event Action WinState;
     private bool hasWon;
+    private ParticleSystem brickbreak;
+
+    private void Awake()
+    {
+        brickbreak = transform.parent.Find("Brickbreak").GetComponent<ParticleSystem>();
+    }
+    GameObject VictoryTouch(Collider2D collision)
+    {
+        GameObject bird = collision.gameObject;
+        bird.GetComponent<BirdMovement>().enabled = false;
+        bird.gameObject.GetComponent<Rigidbody2D>().velocity = Vector3.zero;
+        bird.gameObject.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
+        hasWon = true;
+        WinState?.Invoke();
+        print("here");
+        return bird;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (!collision.gameObject.CompareTag("Player") || hasWon)
+        {
+            return;
+        }
+        GameObject bird = VictoryTouch(collision.collider);
+        LaunchIn(bird.transform);
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -16,16 +43,8 @@ public class PipeExit : MonoBehaviour
         {
            return;
         }
-        GameObject bird = collision.gameObject;
-        bird.GetComponent<BirdMovement>().enabled = false;
-        bird.gameObject.GetComponent<Rigidbody2D>().velocity = Vector3.zero;
-        bird.gameObject.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
-
+        GameObject bird = VictoryTouch(collision);
         StartCoroutine(MoveIn(bird.transform));
-        
-        hasWon = true;
-        WinState?.Invoke();
-        print("here");
     }
 
     IEnumerator MoveIn(Transform trans)
@@ -35,7 +54,30 @@ public class PipeExit : MonoBehaviour
             trans.DOMove(trans.position + new Vector3(0.2f, 0, 0), 0.15f);
             yield return new WaitForSeconds(0.3f);
         }
+        WinStage();
+    }
+
+    void WinStage()
+    {
         SceneManager.LoadScene(0);
-        print("moved");
+    }
+
+    void LaunchIn(Transform trans)
+    {
+        trans.DOMove(trans.position + new Vector3(3f, 0, 0), 1f);
+        var bPos = brickbreak.transform.position;
+        bPos.y = trans.position.y;
+        brickbreak.transform.position = bPos;
+        StartCoroutine(Emit());
+        Invoke("WinStage", 3);
+    }
+
+    IEnumerator Emit()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            brickbreak.Emit(UnityEngine.Random.Range(3, 8));
+            yield return new WaitForSeconds(0.1f);
+        }
     }
 }
